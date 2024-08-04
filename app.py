20af3dd033503db5104e4061bc5bb7a5bdeef2f3
Rohit <rk2289868@gmail.com>
2024-08-04 11:11:04 +0530

    initiated the webpage with some buttons

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header("MLOps")
+
+st.sidebar.title("MLOps Topics")
+st.sidebar.button("Machine Learning")
+st.sidebar.button("Deep Learning")
+st.sidebar.button("AWS")
+st.sidebar.button("Data Science")
+

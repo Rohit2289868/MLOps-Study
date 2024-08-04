ba81e38d5c87270445c2e25e0609bcb2a302e938
Rohit <rk2289868@gmail.com>
2024-08-04 15:08:33 +0530

    ML course details added

--- app.py
+++ app.py
@@ -3,8 +3,23 @@ import streamlit as st
 st.header("MLOps")
 
 st.sidebar.title("MLOps Topics")
-st.sidebar.button("Machine Learning")
-st.sidebar.button("Deep Learning")
-st.sidebar.button("AWS")
-st.sidebar.button("Data Science")
+ml = st.sidebar.button("Machine Learning")
+dl = st.sidebar.button("Deep Learning")
+aws = st.sidebar.button("AWS")
+ds = st.sidebar.button("Data Science")
+
+if ml:
+    st.header("Machine Learning")
+    st.subheader("Course Syllabus")
+    st.write("""
+        - Introduction to Machine Learning
+        - Supervised Learning
+        - Unsupervised Learning
+        - Reinforcement Learning
+        - Feature Engineering
+        - Model Evaluation and Selection
+        - Deployment of Machine Learning Models
+        """)
+    st.subheader("Course Price")
+    st.write("₹5000")
 

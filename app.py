3223b3400a2c71fd8456fa7861f52b75a71c644e
Rohit <rk2289868@gmail.com>
2024-08-06 08:58:00 +0530

    syllabus and course price of deep learning and AWS is added

--- app.py
+++ app.py
@@ -23,3 +23,32 @@ if ml:
     st.subheader("Course Price")
     st.write("₹5000")
 
+if dl:
+    st.header("Deep Learning")
+    st.subheader("Course Syllabus")
+    st.write("""
+        - Introduction to Deep Learning
+        - Neural Networks
+        - Convolutional Neural Networks (CNN)
+        - Recurrent Neural Networks (RNN)
+        - Long Short-Term Memory (LSTM)
+        - Generative Adversarial Networks (GAN)
+        - Deep Learning Model Deployment
+        """)
+    st.subheader("Course Price")
+    st.write("₹7000")
+
+if aws:
+    st.header("AWS")
+    st.subheader("Course Syllabus")
+    st.write("""
+        - Introduction to AWS
+        - AWS Cloud Services
+        - AWS Machine Learning Services
+        - Setting Up AWS Environment
+        - Building and Deploying Models on AWS
+        - Monitoring and Managing Models on AWS
+        """)
+    st.subheader("Course Price")
+    st.write("₹6000")
+
